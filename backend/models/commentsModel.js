59bd82cf5239b1077cdb1cefa6ba6da569081440
Anurag-Band <anuragband10@gmail.com>
2023-02-23 16:40:53 +0530

    made changes in comment schema

--- backend/models/commentsModel.js
+++ backend/models/commentsModel.js
@@ -2,26 +2,42 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
 const commentsSchema = mongoose.Schema(
-   {
-      video: {
-         type: ObjectId,
-         ref: 'videos',
-         required: [true, 'Please provide video informations'],
+  {
+    video: {
+      type: ObjectId,
+      ref: 'videos',
+      required: [true, 'Please provide video informations'],
+    },
+    main: {
+      user: {
+        type: ObjectId,
+        ref: 'users',
+        required: [true, 'Please provide user informations'],
       },
-      main: {
-         type: ObjectId,
-         ref: 'baseComments',
+      content: {
+        type: String,
+        trim: true,
+        required: [true, 'Please provide comment content'],
       },
-      replies: [
-         {
-            type: ObjectId,
-            ref: 'baseComments',
-         },
-      ],
-   },
-   {
-      timestamps: true,
-   }
+    },
+    replies: [
+      {
+        user: {
+          type: ObjectId,
+          ref: 'users',
+          required: [true, 'Please provide user informations'],
+        },
+        content: {
+          type: String,
+          trim: true,
+          required: [true, 'Please provide comment content'],
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = commentsSchema;
